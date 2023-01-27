ac03a5992ca4ebae21c3bdfb70a014bbd9b2c733
potopus <paveldelo@yandex.ru>
2023-01-27 11:11:42 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined) return [];
+  return matrix.length === 0  ? [] : matrix.map((item, count) => count % 2 === 0 ? item : item = item.reverse()).flat();
 }
+
+
+// const matrix = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16],
+// ];
+
+// console.log(towelSort())
